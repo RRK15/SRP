9baced82d1b36321f69ce9e330116242a65bd010
sjb4243 <sambennett4243@gmail.com>
2023-04-22 14:19:40 +0100

    Changes file structure + cleans up and adds new features to website

--- shiny/app.R
+++ shiny/app.R
@@ -6,11 +6,17 @@ library(dplyr)
 library(mclust)
 library(Rtsne)
 
-testdata <- read.csv("cluster_genes.csv")
-metadata <- read.csv("SraRunTableMod.csv", row.names = 1)
+
+
+altclust <- read.csv("data/cluster_genes.csv")
+
+metadata <- read.csv("data/SraRunTableMod.csv", row.names = 1)
 features <-subset(metadata, select = c(Cell_type))
 
-distance_matrix <- read.csv("distance_matrix.csv", row.names = "X")
+distance_matrix <- read.csv("data/distance_matrix.csv", row.names = "X")
+
+altdata <- read.csv("data/alternate.csv")
+
 #replace all nans with 0
 distance_matrix[is.na(distance_matrix)] <- 0
 distance_matrix <- as.matrix(distance_matrix)
@@ -27,12 +33,42 @@ testing$mclust = factor(clustering$classification)
 
 ui <- dashboardPage(
   dashboardHeader(title = "Basic dashboard"),
-  dashboardSidebar(),
+  sidebar <- dashboardSidebar(
+    sidebarMenu(
+      menuItem("Welcome!", tabName = "welcome", icon = icon("dashboard")),
+      menuItem("Original pipeline", icon = icon("th"), tabName = "originalpipeline"),
+      menuItem("Alternate pipeline", icon = icon("th"), tabName = "alternatepipeline")
+    )
+  ),
+  
   dashboardBody(
-    box("Top 20 enriched genes in cluster",uiOutput("testing"), width = 4, height = 8),
-    box(plotlyOutput("mclustplot"), width = 8)
-  )
-  )
+    tabItems(
+      tabItem(tabName = "originalpipeline",
+    fluidRow(
+      column(width = 8,
+             box(plotlyOutput("mclustplot"), width = NULL)
+      ),
+      column(width = 4,
+             box("Top 10 enriched genes in cluster:",uiOutput("PLS"), width = NULL),
+             
+      )
+    )
+  ),
+      tabItem(tabName = "alternatepipeline",
+        fluidRow(
+          column(width = 8,
+              box(plotlyOutput("alternateplot"), width = NULL)
+          ),
+          column(width = 4,
+              box("Top 10 enriched genes in cluster:", uiOutput("altclust"), width = NULL),
+              box(textInput("querygene",label = "Query a gene"), width = NULL),
+              box(textOutput("geneoutput"), width = NULL)
+          )
+        )
+)
+)
+)
+)
 
 server <- function(input, output) {
   val = reactiveVal(1)
@@ -43,23 +79,64 @@ server <- function(input, output) {
     
   })
   output$mclustplot <- renderPlotly({
-    plot_ly(testing, x = ~tsne1, y = ~tsne2, 
-            color = ~Cell_type,
-            colors = "Set3",
-            type = "scatter",
-            mode = "markers",
-            source = "testing",
-            customdata = ~mclust,
-            legend = FALSE,
-            text = ~paste("Cluster ID:", mclust, 
-                          "<br>Cell Type:", Cell_type),
-            hoverinfo = "text")
-    
+    testing %>%
+      highlight_key(~mclust) %>%
+      plot_ly(x = ~tsne1, y = ~tsne2, 
+              color = ~Cell_type,
+              colors = "Set3",
+              type = "scatter",
+              mode = "markers",
+              source = "testing",
+              customdata = ~mclust,
+              text = ~paste("Cluster ID:", mclust, 
+                            "<br>Cell Type:", Cell_type),
+              hoverinfo = "text")%>%
+      highlight(on = "plotly_hover", off = "plotly_doubleclick") 
   })
   
- 
-  output$testing <- renderUI({
+  observe({
+    click_data <- event_data(event = "plotly_click", source = "altdata")
+    clickval <- as.integer(click_data$key) 
+    val(clickval)
+
+  })
+  
+  output$altclust <- renderUI({
+    altclust[val(),]
+  })
+  
+  output$alternateplot <- renderPlotly({
+    altdata %>%
+      highlight_key(~clusters) %>%
+      plot_ly(x = ~UMAP_1, y = ~UMAP_2, 
+              type = 'scatter', 
+              color = ~Cell_type, 
+              colors = "Set3",
+              source = "altdata",
+              mode = 'markers',  
+              text = ~paste("Cluster ID:", clusters,"<br>Cell Type:", Cell_type), 
+              hoverinfo = "text") %>%
+      highlight(on = "plotly_hover", off = "plotly_doubleclick")
+    
+  })
+  output$PLS <- renderUI({
     testdata[val(),]
+  })
+  
+  querytest <- reactive({
+    req(input$querygene)
+    grepstuff = grep(input$querygene, altclust$genes)
+    if (identical(grepstuff, integer(0))){
+      print("No genes found")
+      
+    }else{
+      results = paste(grep(input$querygene, altclust$genes), collapse = " ")
+      paste("Genes found in clustert(s):",results, collapse = " ")
+    }
+  })
+  
+  output$geneoutput <- renderText({
+    querytest()
   })
 } 
 
